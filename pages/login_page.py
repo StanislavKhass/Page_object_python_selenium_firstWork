476157fd46af95c11615d47238e3da681ab3d000
stas khass <stas.khass@test.ru>
2023-04-22 12:21:47 +0300

    add login page method check

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .main_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        current_url = self.browser.current_url
+        # реализуйте проверку на корректный url адрес
+        if "login" not in current_url:
+            assert False , "Url should have '/login' in it"
+        else:
+            assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REG_FORM), "Registration form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,15 +1,17 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import time
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    time.sleep(10)
-    
+    page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url) #создаем обьект для работы с страницей логина
+    login_page.should_be_login_page() # Проверяем 3 условия - 1.есть login в ссылке , 2.есть формы логина и 3.регистрации
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()    
+    page.should_be_login_link()
